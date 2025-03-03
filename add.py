3e9153b159ce503f97713312ea5b91a0116f2a79
preetamkurella <preetamkurella@gmail.com>
2025-03-03 13:19:52 +0530

    added subtraction to the program

--- add.py
+++ add.py
@@ -3,3 +3,7 @@ b = 44
 c = a + b
 print(c)
 # What is the value of c after the code above is executed?
+# new subtraction
+d = a - b
+print(d)
+# What is the value of d after the code above is executed?
